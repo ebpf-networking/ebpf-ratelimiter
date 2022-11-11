a2b8cc53dd4b25f7b0326158d12f2173719140a8
Dushyant Behl <dushyantbehl@in.ibm.com>
2022-11-11 14:19:39 +0530

    add loadgen argument

--- loadgen/packet_crafter.py
+++ loadgen/packet_crafter.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 import time
+import argparse
 
 def allow_connections(num,sleep_time):
     print("Allow Connections")
@@ -25,18 +26,17 @@ def deny_connections(num):
 if __name__ == "__main__":
     src_ip = "40.0.0.2"
     dst_ip = "40.0.0.1"
-    dport = 443
+    dport = 8080
     pcount = 3000
     scount = 12345
-    #deny_connections(100)
-    allow_connections(10,2)
+    parser = argparse.ArgumentParser(description='packet loadgen for rate limiter')
 
-'''
-for i in range(100):
-    ip=IP(src=src_ip,dst=dst_ip)  
-    SYN=TCP(sport=pcount,dport=dport,flags="S",seq=scount)
-    send(ip/SYN)
-    scount = scount + 1
-    pcount = pcount + 1
-    time.sleep(2)
-'''
+    parser.add_argument("-m", "--mode", help="Test limited connection or flood allow/deny", type=str)
+    args = parser.parse_args()
+
+    if args.mode == 'allow':
+        allow_connections(10, 2)
+    elif args.mode == 'deny':
+        deny_connections(100)
+    else:
+        print("Unknown mode for packet crafter. Please pass allow/deny\n")
